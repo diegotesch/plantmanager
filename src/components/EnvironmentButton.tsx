d1d46df79bbb6aba83b9e72b2b75084bc03b38f1
Diego Tesch <diego.tesch@gmail.com>
2021-04-23 11:59:57 -0300

    nlw#5 - dia 3 - consumo de api paginacao ordenacao e filtro

--- src/components/EnvironmentButton.tsx
+++ src/components/EnvironmentButton.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
   StyleSheet,
-  View,
   Text
 } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface EnvironmentButtonProps extends RectButtonProps {
   title: string;
@@ -15,18 +17,47 @@ export function EnvironmentButton({
   title,
   active = false,
   ...rest
- }) {
+ }: EnvironmentButtonProps) {
   return (
-    <View>
-      <Text>
-        Botão
+    <RectButton 
+      style={[
+        styles.container,
+        active && styles.containerActive
+      ]}
+      {...rest}
+    >
+      <Text 
+        style={[
+          styles.text,
+          active && styles.textAtive
+        ]}
+      >
+        { title }
       </Text>
-    </View>
+    </RectButton>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: colors.shape,
+    width: 70,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+    marginHorizontal: 5
   },
+  containerActive: {
+    backgroundColor: colors.green_light,
+  },
+  text: {
+    color: colors.heading,
+    fontFamily: fonts.text,
+    fontSize: 11
+  },
+  textAtive: {
+    fontFamily: fonts.heading,
+    color: colors.green_dark,
+  }
 });
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,18 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   SafeAreaView,
   View,
-  Text
+  Text,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native';
 
+import { EnvironmentButton } from '../components/EnvironmentButton';
 import { Header } from '../components/Header'
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import { Load } from '../components/Load';
+
+import api from '../services/api';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
-export function PlantSelect() {
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
+interface PlantProps {
+  id: string;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: [string];
+  frequency: {
+    times: number;
+    repeat_every: string;
+  }
+}
 
+export function PlantSelect() {
+  const [environment, setEnvironment] = useState<EnvironmentProps[]>([]);
+  const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+  const [environmentSelected, setEnvironmentSelected] = useState('all');
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [loadingMode, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
+
+  function handleEnvironmentSelected(environemnt: string) {
+    setEnvironmentSelected(environemnt);
+
+    if (environemnt == 'all') 
+      return setFilteredPlants(plants);
+
+    const filtered = plants.filter(plant => (
+      plant.environments.includes(environemnt)
+    ));
+
+    setFilteredPlants(filtered);
+  }
+
+  async function fetchPlants() {
+    const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+    if (!data)
+      return setLoading(true);
+
+    if (page > 1) {
+      setPlants(oldValue => [...oldValue, ...data])
+      setFilteredPlants(oldValue => [...oldValue, ...data])
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  function handleFetchMore(distance: number) {
+    if (distance < 1)
+      return;
+
+    setLoadingMore(true);
+    setPage(oldValue => oldValue + 1);
+    fetchPlants();
+  }
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get('plants_environments?_sort=title&_order=asc');
+      setEnvironment([
+        {
+          key: 'all',
+          title: 'Todos',
+        }, 
+        ...data
+      ]);
+    }
+
+    fetchEnvironment();
+  }, []);
+
+  useEffect(() => {
+    fetchPlants();
+  }, [])
+
+  if(loading)
+    return <Load />
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -24,6 +118,44 @@ export function PlantSelect() {
           você quer colocar sua planta?
         </Text>
       </View>
+
+      <View>
+        <FlatList
+          data={environment}
+          renderItem={({ item }) => (
+            <EnvironmentButton 
+              title={item.title} 
+              active={item.key === environmentSelected}
+              onPress={() => handleEnvironmentSelected(item.key)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
+      </View>
+
+      <View style={styles.plants}>
+        <FlatList
+          data={filteredPlants}
+          renderItem={({ item }) => (
+            <PlantCardPrimary
+              data={item}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          onEndReachedThreshold={0.1}
+          onEndReached={({ distanceFromEnd }) => handleFetchMore(distanceFromEnd)}
+          ListFooterComponent={
+            loadingMode 
+            ? <ActivityIndicator color={colors.green} />
+            : <></>
+          }
+          contentContainerStyle={styles.contentContainerStyle}
+        />
+      </View>
+      
     </SafeAreaView>
   );
 }
@@ -34,7 +166,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
   },
   header: {
-    paddingHorizontal: 30
+    paddingHorizontal: 10
   },
   title: {
     fontSize: 15,
@@ -47,5 +179,20 @@ const styles = StyleSheet.create({
     fontSize: 15,
     lineHeight: 18,
     color: colors.heading
+  },
+  environmentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginHorizontal: 10,
+    marginVertical: 32
+  },
+  plants: {
+    flex: 1,
+    paddingHorizontal: 32,
+    justifyContent: 'center',
+  },
+  contentContainerStyle: {
+    
   }
 })
